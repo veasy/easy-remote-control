1dc151c3f29f153649f8d55ba76475e8c8364b4d
Florian <florian@nexpose.ch>
2015-08-08 10:54:29 +0200

    implemented webserver with socketio as base service

--- erc-server/erc_server.py
+++ erc-server/erc_server.py
@@ -1,7 +1,6 @@
 import erc_config
-from erc_server import app
+from erc_server import socketio, app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=erc_config.ERC_SERVER_PORT,
-            debug=erc_config.ERC_SERVER_DEBUG_MODE)
+    socketio.run(app=app, host='0.0.0.0',
+                 port=erc_config.ERC_SERVER_PORT)
--- erc-server/erc_server/__init__.py
+++ erc-server/erc_server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.socketio import SocketIO
 
 import erc_config
 
@@ -6,6 +7,8 @@ app = Flask(__name__,
             static_folder=erc_config.ERC_SERVER_STATIC_FOLDER,
             static_url_path=erc_config.ERC_SERVER_STATIC_PATH,
             template_folder=erc_config.ERC_SERVER_TEMPLATE_FOLDER)
+
+socketio = SocketIO(app)
 
 # import routes
 import routes.app_routes
