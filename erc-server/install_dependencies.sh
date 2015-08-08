d9b792a3a51151396004ba2df20be5d629495cde
Florian <florian@nexpose.ch>
2015-08-08 19:56:40 +0200

    fixed dependencies

--- erc-server/install_dependencies.sh
+++ erc-server/install_dependencies.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
+
 if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
     echo "install autopy on MacOSX..."
     # specialized install of autopy
     brew install libpng
     CFLAGS="-Wno-return-type" pip install git+https://github.com/potpath/autopy.git
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+    apt-get install libxtst-dev
+    apt-get install libpng-dev zlib1g-dev
+
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+    echo "nothing to install here..."
 fi
 
 pip install -r requirements.txt
