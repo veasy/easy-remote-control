c96e99c28119d2dac95efc50c421c90868ae7335
Florian <florian@nexpose.ch>
2015-08-08 14:22:40 +0200

    speedup movement because of only loading libx11 at the beginning

--- erc-server/erc_server/unified_control/mouse/x_mouse.py
+++ erc-server/erc_server/unified_control/mouse/x_mouse.py
@@ -1,13 +1,12 @@
 from _ctypes import byref
 from ctypes import cdll, c_uint32, c_int, c_uint
 
+xlib = cdll.LoadLibrary('libX11.so')
+d = xlib.XOpenDisplay(None)
+w = xlib.XDefaultRootWindow(d)
 
 def move_mouse(x, y):
-    dll = cdll.LoadLibrary('libX11.so')
-    d = dll.XOpenDisplay(None)
-    root = dll.XDefaultRootWindow(d)
-    dll.XWarpPointer(d, None, root, 0, 0, 0, 0, int(x), int(y))
-    dll.XCloseDisplay(d)
+    xlib.XWarpPointer(d, None, w, 0, 0, 0, 0, int(x), int(y))
 
 
 def move_mouse_relative(dx, dy):
@@ -17,19 +16,17 @@ def move_mouse_relative(dx, dy):
 
 
 def get_mouse_position():
-    dll = cdll.LoadLibrary('libX11.so')
-    d = dll.XOpenDisplay(None)
-    w = dll.XDefaultRootWindow(d)
-
     (root_id, child_id) = (c_uint32(), c_uint32())
     (root_x, root_y, win_x, win_y) = (c_int(), c_int(), c_int(), c_int())
     mask = c_uint()
-    dll.XQueryPointer(d, c_uint32(w), byref(root_id), byref(child_id),
+    xlib.XQueryPointer(d, c_uint32(w), byref(root_id), byref(child_id),
                       byref(root_x), byref(root_y),
                       byref(win_x), byref(win_y), byref(mask))
 
     # print 'Result: %s' % result
     # print 'Coordinates: %s, %s' % (root_x, root_y)
 
-    dll.XCloseDisplay(d)
     return root_x, root_y
+
+def close_display():
+    xlib.XCloseDisplay(d)
